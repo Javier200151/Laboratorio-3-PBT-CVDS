edad86a1aa7198bbdbb655ac31ce16bd19d82e80
Andres Davila <oadavilaf18@gmail.com>
2020-08-28 18:42:44 -0500

    se realizaron pruebas registry

--- ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
@@ -15,11 +15,47 @@ public class RegistryTest {
     @Test
     public void validateRegistryResult() {
 
-        Person person = new Person();
+        Person person = new Person("Andres Davila", 1032496251, 22,Gender.MALE, true);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.VALID, result);
+    }
+    @Test
+    public void validateRegistryDead() {
+
+        Person person = new Person("MIchael jackson",447017832,45,Gender.MALE,false);
 
         RegisterResult result = registry.registerVoter(person);
 
-        Assert.assertEquals(RegisterResult.VALID, result);
+        Assert.assertEquals(RegisterResult.DEAD, result);
+    }
+    @Test
+    public void validateRegistryDuplicated() {
+
+        Person person = new Person("Andres Davila", 1032496251, 22,Gender.MALE, true);
+
+        RegisterResult result = registry.registerVoter(person);
+
+        result = registry.registerVoter(person);
+
+        Assert.assertEquals(RegisterResult.DUPLICATED, result);
+    }
+    @Test
+    public void validateRegistryUnderAge() {
+
+        Person person = new Person("elver galarga",1001520057,10,Gender.MALE,true);
+
+        RegisterResult result = registry.registerVoter(person);
+
+        Assert.assertEquals(RegisterResult.UNDERAGE, result);
+    }
+    @Test
+    public void validateRegistryValidAge() {
+
+        Person person = new Person("Reina Isabelle",549675,138,Gender.FEMALE,true);
+
+        RegisterResult result = registry.registerVoter(person);
+
+        Assert.assertEquals(RegisterResult.INVALID_AGE, result);
     }
 
     // TODO Complete with more test cases
