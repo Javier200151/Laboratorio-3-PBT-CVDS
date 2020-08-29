22d379dfe17b42b7af323972fd3370a7069c8891
Javier200151 <javier200151@gmail.com>
2020-08-28 21:29:40 -0500

    Pruebas Aerodescuentos

--- ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
@@ -75,5 +75,255 @@ public class TarifasTest {
         }
     }
     
-    
+    @Test
+    public void pruebaTarifa2()
+    {
+        double ans = calculadora.calculoTarifa(500,6,25);
+        double res= 500;
+        
+        Assert.assertEquals(ans,res,0.0f);
+    }
+     @Test
+    public void pruebaDiasAntelacion1() {
+        
+        try{
+            double res = calculadora.calculoTarifa(800,-15,30);
+        }catch(Exception e){
+            Assert.assertTrue(true);
+        }
+        
+    }
+    @Test
+    public void pruebaDiasAntelacion2() {
+        
+        
+        double res = calculadora.calculoTarifa(800,8,30);
+        double cor =800;
+        
+        Assert.assertEquals(res,cor,0.0f);
+        
+        
+    }
+    @Test
+    public void pruebaDiasAntelacion3() {
+        
+        
+        double res = calculadora.calculoTarifa(800,29,30);
+        double cor =680;
+        
+        Assert.assertEquals(res,cor,0.0f);
+        
+        
+    }
+    @Test
+    public void pruebaDiasAntelacion4() {
+        
+        try{
+            double res = calculadora.calculoTarifa(800,10000,30);
+        }catch(Exception e){
+            Assert.assertTrue(true);
+        }
+        
+    }
+    @Test
+    public void pruebaFronteraEdad1() {
+        
+        try{
+            double res = calculadora.calculoTarifa(500,10,-1);
+        }catch(Exception e){
+            Assert.assertTrue(true);
+        }
+        
+    }
+    @Test
+    public void pruebaFronteraEdad2() {
+        
+        double res = calculadora.calculoTarifa(500,10,0);
+        double cor =475;
+        
+        Assert.assertEquals(res,cor,0.0f);
+        
+    }
+    @Test
+    public void pruebaFronteraEdad3() {
+        
+        double res = calculadora.calculoTarifa(500,10,1);
+        double cor =475;
+        
+        Assert.assertEquals(res,cor,0.0f);
+        
+    }
+    @Test
+    public void pruebaFronteraEdad4() {
+        
+        double res = calculadora.calculoTarifa(500,10,17);
+        double cor =475;
+        
+        Assert.assertEquals(res,cor,0.0f);
+        
+    }
+    @Test
+    public void pruebaFronteraEdad5() {
+        
+        double res = calculadora.calculoTarifa(500,10,18);
+        double cor =500;
+        
+        Assert.assertEquals(res,cor,0.0f);
+        
+    }
+    @Test
+    public void pruebaFronteraEdad6() {
+        
+        double res = calculadora.calculoTarifa(500,10,19);
+        double cor =500;
+        
+        Assert.assertEquals(res,cor,0.0f);
+        
+    }
+    @Test
+    public void pruebaFronteraEdad7() {
+        
+        double res = calculadora.calculoTarifa(500,10,64);
+        double cor =500;
+        
+        Assert.assertEquals(res,cor,0.0f);
+        
+    }
+    @Test
+    public void pruebaFronteraEdad8() {
+        
+        double res = calculadora.calculoTarifa(500,10,65);
+        double cor =500;
+        
+        Assert.assertEquals(res,cor,0.0f);
+        
+    }
+    @Test
+    public void pruebaFronteraEdad9() {
+        
+        double res = calculadora.calculoTarifa(500,10,66);
+        double cor =460;
+        
+        Assert.assertEquals(res,cor,0.0f);
+        
+    }
+    @Test
+    public void pruebaFronteraEdad10() {
+        
+        double res = calculadora.calculoTarifa(500,10,109);
+        double cor =460;
+        
+        Assert.assertEquals(res,cor,0.0f);
+        
+    }
+    @Test
+    public void pruebaFronteraEdad11() {
+        
+        double res = calculadora.calculoTarifa(500,10,110);
+        double cor =460;
+        
+        Assert.assertEquals(res,cor,0.0f);
+        
+    }
+    @Test
+    public void pruebaFronteraEdad12() {
+        
+        try{
+            double res = calculadora.calculoTarifa(500,10,111);
+        }catch(Exception e){
+            Assert.assertTrue(true);
+        }
+        
+    }
+    @Test
+    public void pruebaFronteraTarifa1() {
+        
+        try{
+            double res = calculadora.calculoTarifa(-1,10,25);
+        }catch(Exception e){
+            Assert.assertTrue(true);
+        }
+        
+    }
+    @Test
+    public void pruebaFronteraTarifa2() {
+        
+        double res = calculadora.calculoTarifa(0,10,25);
+        double cor =0;
+        
+        Assert.assertEquals(res,cor,0.0f);
+        
+    }
+    @Test
+    public void pruebaFronteraDiasAntelacion1() {
+        
+        try{
+            double res = calculadora.calculoTarifa(500,-1,25);
+        }catch(Exception e){
+            Assert.assertTrue(true);
+        }
+        
+    }
+    @Test
+    public void pruebaFronteraDiasAntelacion2() {
+        
+        double res = calculadora.calculoTarifa(500,0,25);
+        double cor =500;
+        
+        Assert.assertEquals(res,cor,0.0f);
+        
+    }
+    @Test
+    public void pruebaFronteraDiasAntelacion3() {
+        
+        double res = calculadora.calculoTarifa(500,1,25);
+        double cor =500;
+        
+        Assert.assertEquals(res,cor,0.0f);
+        
+    }
+    @Test
+    public void pruebaFronteraDiasAntelacion4() {
+        
+        double res = calculadora.calculoTarifa(500,19,25);
+        double cor =500;
+        
+        Assert.assertEquals(res,cor,0.0f);
+        
+    }
+    @Test
+    public void pruebaFronteraDiasAntelacion5() {
+        
+        double res = calculadora.calculoTarifa(500,20,25);
+        double cor =500;
+        
+        Assert.assertEquals(res,cor,0.0f);
+        
+    }
+    @Test
+    public void pruebaFronteraDiasAntelacion6() {
+        
+        double res = calculadora.calculoTarifa(500,21,25);
+        double cor =425;
+        
+        Assert.assertEquals(res,cor,0.0f);
+        
+    }
+    @Test
+    public void pruebaFronteraDiasAntelacion7() {
+        
+        double res = calculadora.calculoTarifa(500,9999,25);
+        double cor =425;
+        Assert.assertEquals(res,cor,0.0f);
+        
+    }
+    @Test
+    public void pruebaFronteraDiasAntelacion8() {
+        
+        try{
+            double res = calculadora.calculoTarifa(500,10000,25);
+        }catch(Exception e){
+            Assert.assertTrue(true);
+        }
+    }
 }
